ac7e8c0bdab15cbcb5946cf00e9972b7aba26f0f
geethz2908 <geethikasamayamantula@gmail.com>
2025-11-11 23:11:04 +0530

    Ready for frontend deployment

--- frontend/app.py
+++ frontend/app.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
-BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:8000")
+BACKEND_URL = os.getenv("BACKEND_URL")
 
 # --- Streamlit Page Configuration ---
 st.set_page_config(page_title="Olist Chat", layout="wide")
